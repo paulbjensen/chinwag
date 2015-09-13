db4808bc6df5979850cebacf911b8fb26f29d01b
Paul Jensen <paulbjensen@gmail.com>
2015-09-13 10:28:11 +0100

    Added an example config file

---
+++ config.example.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+
+// The configuration object
+//
+var config = {
+	peerjs: {
+		key: 'YOUR_KEY_HERE'
+	}
+};
+
+
+
+// Expose the module as the public API
+//
+module.exports = config;
